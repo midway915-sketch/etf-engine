6f95d4f622f558a590edc45affe8b03e2f40d9ee
midway915-sketch <midway915@gmail.com>
2026-02-14 20:33:51 +0900

    Update universe.py

--- app/universe.py
+++ app/universe.py
@@ -1,11 +1,32 @@
+# 3x Leveraged ETF Universe
+
 UNIVERSE = [
-    "SPY",
-    "QQQ",
-    "IWM",
-    "XLK",
-    "XLF",
-    "XLE",
-    "XLV"
+    "SOXL",  # 반도체 3x
+    "BULZ",  # 기술주 3x
+    "TQQQ",  # 나스닥 3x
+    "TECL",  # 기술 섹터 3x
+    "WEBL",  # 인터넷 3x
+    "UPRO",  # S&P500 3x
+    "WANT",  # 소비재 3x
+    "HIBL",  # 고베타 3x
+    "FNGU",  # FANG 3x
+    "TNA",   # 러셀2000 3x
+    "RETL",  # 리테일 3x
+    "UDOW",  # 다우 3x
+    "NAIL",  # 홈빌더 3x
+    "LABU",  # 바이오 3x
+    "PILL",  # 헬스케어 3x
+    "MIDU",  # 미드캡 3x
+    "CURE",  # 헬스케어 3x
+    "FAS",   # 금융 3x
+    "TPOR",  # 교통 3x
+    "DRN",   # 리츠 3x
+    "DUSL",  # 산업 3x
+    "DFEN",  # 항공방산 3x
+    "UTSL",  # 유틸리티 3x
+    "BNKU",  # 은행 3x
+    "DPST"   # 지역은행 3x
 ]
 
+# 시장 필터 (기준 ETF)
 MARKET_ETF = "SPY"
